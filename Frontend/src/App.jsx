bd09fc70736cf2d0f17521d71526ad219bdd0d97
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-09-01 21:59:43 -0400

    Added customized colors to tailwind.config file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,21 +4,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Routes from "./routes/Routes";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-
-=======
 import Dashboard from "./screens/Volunteer/Dashboard/VolunteerApplicantsDashboard";
 import VolunteerApplicantsDashboard from "./screens/Volunteer/Dashboard/VolunteerApplicantsDashboard";
->>>>>>> 3c1dc39a0edd5df98c230ef86fe15a89b4282577
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-     <Navbar />
-=======
       <Navbar />
->>>>>>> 3c1dc39a0edd5df98c230ef86fe15a89b4282577
       <Routes />
       <Footer />
     
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
  
   theme: {
-<<<<<<< HEAD
       colors: {
         'yellow': '#DCB03F',
         'red': '#A5322B',
@@ -14,11 +13,5 @@ export default {
         'gray-light': '#d3dce6',
       },
     },
-=======
-    backgroundImage: {
-      volunteer: "url('./assets/volunteer.jpg')",
-    },
-  },
->>>>>>> 3c1dc39a0edd5df98c230ef86fe15a89b4282577
   plugins: [require("daisyui")],
 };
